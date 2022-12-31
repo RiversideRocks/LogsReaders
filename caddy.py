8d3d218f23366c339e1b6945a836a0e3d5b79306
RiversideRocks <wilest7@gmail.com>
2022-12-31 18:45:52 -0500

    cleanup

--- caddy.py
+++ caddy.py
@@ -3,4 +3,4 @@ import json
 
 for line in sys.stdin:
     a = json.loads(line.rstrip())
-    print(a["request"]["remote_ip"] + " - - \"" + a["request"]["method"] + " " + a["request"]["uri"] + " "  + a["request"]["proto"] + "\" - " + str(a["status"]) + " - \"" + a["request"]["headers"]["User-Agent"][0] + "\" \"" + a["request"]["host"])
+    print(a["request"]["remote_ip"] + " - - \"" + a["request"]["method"] + " " + a["request"]["uri"] + " "  + a["request"]["proto"] + "\" - " + str(a["status"]) + " - \"" + a["request"]["headers"]["User-Agent"][0] + "\" \"" + a["request"]["host"] + "\"")
