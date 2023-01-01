5621772295f4189ba951f9379b93852df8886175
RiversideRocks <wilest7@gmail.com>
2022-12-31 20:51:13 -0500

    search for govs

---
+++ stdin-search-isp.py
@@ -0,0 +1,25 @@
+from apachelogs import LogParser
+import sys
+import requests
+
+
+stor = []
+
+for line in sys.stdin:
+    if 'Exit' == line.rstrip():
+        break
+    parser = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
+    entry = parser.parse(line)
+    if entry.remote_host not in stor:
+        """
+        This next segment of code can always be commented out
+        """
+        try:
+            api = requests.get('https://whois-referral.toolforge.org/gateway.py?lookup=true&format=json&ip=' + entry.remote_host).json()
+            for x in api["nets"]["emails"]:
+                if ".gov" in x:
+                    print(line)
+        except:
+            print()
+        stor.append(entry.remote_host)
+        print("== " + str(len(stor)) + " Unique Hits ==")
